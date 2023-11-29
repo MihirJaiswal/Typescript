d46f2e154b920c3d171e46bdd1e5d1e39c58014b
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-29 22:24:10 +0530

    tuples

---
+++ 12/tupels.ts
@@ -0,0 +1,16 @@
+//const user: (string | number)[] = [1,'mj']
+//now this is an array but what if we wanted the array in more
+//precise order like string should always comes before number
+//in that case we nees tuples
+
+let User: [string, number, boolean]
+
+User = ["anshi", 2224, true]
+
+//eg. 2
+
+let rgb: [number, number, number] = [233, 282, 222]
+
+type user = [number, string]
+
+const newuser: user = [112, "ec@gmail.com"]
