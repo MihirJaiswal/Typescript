ce61d25751a2d4a63882ff9b6d9972fec0bb577d
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-29 21:40:31 +0530

    array

---
+++ 10/array.ts
@@ -0,0 +1,20 @@
+const superHeros: string [] = [];
+superHeros.push("batman");
+
+const superPowers: number [] = [];
+superPowers.push(100)
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUser: User[] = []
+
+allUser.push({name: "Anshita", isActive: false})
+
+const MlModel: number [] [] = [
+    [255,55,434],
+    [44,55,66]
+]
+
