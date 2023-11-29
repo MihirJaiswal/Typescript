3b5812c965c9be17232fc5d3e9a21b714c5a5a01
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-29 20:15:30 +0530

    object

---
+++ 07/objects.ts
@@ -0,0 +1,15 @@
+const User = {
+    name: "mihir",
+    email: "mihir@jaiswal",
+    isActive: true
+}
+
+function createUser({name:string, isPaid: boolean}){}
+
+createUser({name: "Anshita", isPaid: true})
+
+function createCourse():{name: string, price: number}{
+    return{name: "react.js", price: 444}
+}
+
+
