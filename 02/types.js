6bc8c77fcea4025016bc8231443cd2121d732945
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-29 18:12:09 +0530

    types

---
+++ 02/types.js
@@ -0,0 +1,21 @@
+/*types
+numbers
+string
+boolean
+null
+undefined
+void
+object
+array
+tuples
+any
+never
+unknown
+etc*/
+// situations
+// A function accepts 2 numbers
+// A function returns a string
+//syntax
+//let variableName: type = value
+var greeting = "Mihir Jaiswal";
+console.log(greeting);
---
+++ 02/types.ts
@@ -0,0 +1,24 @@
+/*types
+numbers
+string
+boolean
+null
+undefined
+void
+object
+array
+tuples
+any
+never
+unknown 
+etc*/
+
+// situations
+// A function accepts 2 numbers
+// A function returns a string
+
+//syntax
+//let variableName: type = value
+
+let greeting: string = "Mihir Jaiswal"
+console.log(greeting);
