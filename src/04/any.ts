77f9e88582ffae7feab03213c20689986c7f4e70
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-29 23:20:06 +0530

    any

---
+++ src/04/any.ts
@@ -0,0 +1,7 @@
+let temp: any;
+
+temp = "string";
+temp = 2;
+temp = true;
+
+//Any is a data type in TypeScript. Any type is used when we deal with third-party programs and expect any variable but we don’t know the exact type of variable.
